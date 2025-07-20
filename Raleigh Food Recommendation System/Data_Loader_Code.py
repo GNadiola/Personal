fe859332d7e7251bd98296f484d485a8d4d94a4f
GNadiola <gnadiola@gmail.com>
2025-07-20 20:09:02 +0000

    PROBLEM SOLVED:)

--- Raleigh Food Recommendation System/Data_Loader_Code.py
+++ Raleigh Food Recommendation System/Data_Loader_Code.py
@@ -4,10 +4,14 @@
 #Imports
 import pandas as pd 
 import numpy as np 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import os
 
 #Restaurant Storage
-restaurants = pd.read_csv('restaurants.csv')
+raw_url = 'https://raw.githubusercontent.com/GNadiola/Personal/96fd8e9f91817b9fc9fffd9f225646d22de71863/Raleigh%20Food%20Recommendation%20System/restaurants.csv'
+restaurants = pd.read_csv(raw_url)
 
-restaurants.head()
+restaurants.head(1)
+
+#Sorting the cities
+restaurants['CITY'].value_counts()
