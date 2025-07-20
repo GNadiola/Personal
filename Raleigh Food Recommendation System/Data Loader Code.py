4da31f7bb90b2c3f2d3dd47eb1fb0dd747760248
GNadiola <gnadiola@gmail.com>
2025-07-20 18:33:39 +0000

    Help:(

--- Raleigh Food Recommendation System/Data Loader Code.py
+++ Raleigh Food Recommendation System/Data Loader Code.py
@@ -8,6 +8,6 @@ import matplotlib.pyplot as plt
 import os
 
 #Restaurant Storage
-restaurants = pd.read_csv('Restaurants_in_Wake_County.csv')
+restaurants = pd.read_csv('restaurants.csv')
 
 restaurants.head()
