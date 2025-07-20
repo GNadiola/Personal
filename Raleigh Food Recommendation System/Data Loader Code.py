33aaddfc559923d93098ea597f8f15c03f7fcb6f
GNadiola <gnadiola@gmail.com>
2025-07-20 18:12:54 +0000

    Recommendation System Start

---
+++ Raleigh Food Recommendation System/Data Loader Code.py
@@ -0,0 +1,11 @@
+#Nadiola Grosvenor
+#Recommendation System for Restaurants
+
+#Imports
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import os
+
+#Restaurant Storage
+restaurants = pd.read_csv()
