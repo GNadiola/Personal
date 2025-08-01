96298c2c7d139ac9966e1f714383d78085fa0d70
GNadiola <gnadiola@gmail.com>
2025-08-01 23:25:32 +0000

    feeling it ou

--- Alice in Wonderland Garden Type Game/Base_Game.cpp
+++ Alice in Wonderland Garden Type Game/Base_Game.cpp
@@ -15,43 +15,73 @@ int main() {
         int choice;
         cin >> choice;
 
-        if (choice == 1) {
+        while (choice == 1) {
             cout << "\nJuly 31, Thursday, 11:42 am.\n";
             cout << "\nLiving Room\n";
-            cout << "\n \n";
             cout << "\nYou have 18 minutes to get to work.\n";
-            cout << "\nHowever, you are missing 3 essential items.\n";
+            cout << "However, you are missing 3 essential items.\n";
             cout << "\nMissing Items:\n";
-            cout << "\n1. Car Keys\n";
-            cout << "\n2. House Keys\n";
-            cout << "\n3. Cell Phone\n";
-            cout << "\nYou have 18 minutes to get to work.\n";
-            cout << "\nWhich item will you find first?\n";
+            cout << "3. Car Keys\n";
+            cout << "4. House Keys\n";
+            cout << "5. Cell Phone\n";
+            cout << "You have 18 minutes to get to work.\n";
+            cout << "Which item will you find first?\n";
 
             int choice;
             cin >> choice;
-            if (choice == 1) {
+            if (choice == 3) {
                 cout << "\nSo you've chosen to find your car keys!\n";
-                cout << "\nWhere did you last see them?\n";
+                cout << "Where did you last see them?\n";
                 cout << "\n...\n";
-                cout << "\nThe garage? Ok, well let's head there.\n";
+                cout << "The garage? Ok, well let's head there.\n";
                 cout << "\n \n";
-                cout << "\nThe Garage\n"
+                cout << "The Garage\n";
                 cout << "\n \n";
-                cout << "\nOk, take a look around! Quickly! You've got to get to work!\n";
-                cout << "\n...i'm hearing an odd sound...\n";
+                cout << "Ok, take a look around! Quickly! You've got to get to work!\n";
+                cout << "...i'm hearing an odd sound...\n";
                 cout << "\nWhere will you look?\n";
-                cout << "\n1. Under the car\n";
-                cout << "\n2. In boxes\n";
-                cout << "\n3. In the yard supplies\n";
+                cout << "1. Under the car\n";
+                cout << "2. In boxes\n";
+                cout << "3. In the yard supplies\n";
                 
                 int choice;
                 cin >> choice;
                 if (choice == 1) {
-                    cout << "\nDid you see anything?\n";
-                    cout << "\nNO? Well keep looking! Time's running out!\n";
+                    cout << "Did you see anything?\n";
+                    cout << "NO? Well keep looking! Time's running out!\n";
+                    continue;
+                }
+                else if (choice == 2) {
+                    cout << "Did you see anything?\n";
+                    cout << "...a key?? Great! Now you can drive to-\n";
+                    cout << "...\n";
+                    cout << "It's not your car key? Ok, Is it the house key?\n";
+                    cout << "What??? Is it your key at all then???\n";
+                    cout << "Ok well, I don't know where it came from, but you have to keep moving. Time's running out and you were already late yesterday!!\n";
+                    cout << "1. Pick up the key\n";
+                    cout << "2. Leave the key\n";
+                }
+                else if (choice == 3) {
+                    cout << "Did you find anything?\n";
+                    cout << "A hole?? Where?\n";
+                    cout << "In the floor under the yard supplies? Let me see...\n";
+                    cout << "Oh WOW- has that always been there? That's big enough to fit two people inside? Where does it lead? The house is on flat ground, and you don't have a basement...\n";
+                    cout << "Anyway, it doesn't matter. You can call an inspector or something as soon as you find your phone. You've gotta keep moving, you're almost late!\n";
+                    cout << "1. Jump in the hole\n";
+                    cout << "2. Keep looking\n";
                 }
 
+            }
+            if (choice == 4) {
+                cout << "\nSo you've chosen to find your house keys!\n";
+                cout << "Where did you last see them?\n";
+                cout << "\n...\n";
+                cout << "The kitchen counter? Ok, well let's head there.\n";
+                cout << "\n \n";
+                cout << "The Kitchen\n";
+                cout << "\n \n";
+                cout << "Ok, take a look around! Quickly! You've got to get to work!\n";
+                cout << "...i'm hearing an odd sound...\n";
             }
         } else if (choice == 2) {
             cout << "\nSee you later!\n";
